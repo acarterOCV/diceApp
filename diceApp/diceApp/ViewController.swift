fda2c230e3a173ba633a3027ba375ed1f056385c
Ashley Carter <acarter@myocv.com>
2019-10-16 16:19:31 -0500

    First commit

---
+++ diceApp/diceApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  diceApp
+//
+//  Created by Ashley Carter on 10/16/19.
+//  Copyright © 2019 OCV, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
